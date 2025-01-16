09a09f9ad8c16ea3af3f01501e9b1ca73180c1ad
Lsy2006-github <lianyang12lol@gmail.com>
2025-01-17 03:29:23 +0800

    changed dataset

--- apple_stock_app.py
+++ apple_stock_app.py
@@ -5,21 +5,57 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 st.write("""
 # Stock Price Prediction App
-This app predicts the **Stock Price** when the market closes!
+This app predicts the **Rental Price** based on some criteria!
 """)
 
 st.sidebar.header('User Input Parameters')
 
 # Function to get user inputs
 def user_input_features():
-    High = st.sidebar.number_input('Highest Price', 1, 400, 23)
-    Low = st.sidebar.number_input('Lowest Price', -10, 400, 21)
-    Open = st.sidebar.number_input('Open Price', -10, 400, 22)
-    data = pd.DataFrame({
-        'Open': [Open],
-        'High': [High],
-        'Low': [Low],
-    })
+    BHK = st.sidebar.slider('Bedroom, Hallway, Kitchen', 1, 5, 2)
+    Size = st.sidebar.slider('Lowest Price', 10, 4000000, 10000)
+    Bathroom = st.sidebar.slider('Open Price', 1, 10, 5)
+    Area_Type = st.sidebar.selectbox('Area Type', ('Built Area', 'Carpet Area', 'Super Area'))
+    City = st.sidebar.selectbox('City', ('Bangalore', 'Mumbai', 'Chennai', 'Hyderabad', 'Kolkata', 'Delhi'))
+    Furnishing_Status = st.sidebar.selectbox('Furnishing Status', ('Semi-Furnished', 'Unfurnished', 'Furnished'))
+    Tenant_Preferred = st.sidebar.selectbox('Tenant Preferred', ('Family', 'Bachelor', 'Company'))
+    Point_of_Contact = st.sidebar.selectbox('Point of Contact', ('Agent', 'Owner', 'Builder'))
+    Floors = st.sidebar.selectbox('The Floor Level of the Building', ('Ground', 'Lower', 'Upper', 'Other'))
+    if Floors == 'Other':
+        Floors = st.sidebar.slider('Floors', 1, 100, 1)
+    else:
+        Floors = Floors
+    data = {
+        'BHK': [BHK],
+        'Size': [Size],
+        'Bathroom': [Bathroom],
+        'Area_Type_Built Area': [1 if Area_Type == 'Built Area' else 0],
+        'Area_Type_Carpet Area': [1 if Area_Type == 'Carpet Area' else 0],
+        'Area_Type_Super Area': [1 if Area_Type == 'Super Area' else 0],
+        'City_Bangalore': [1 if City == 'Bangalore' else 0],
+        'City_Mumbai': [1 if City == 'Mumbai' else 0],
+        'City_Chennai': [1 if City == 'Chennai' else 0],
+        'City_Hyderabad': [1 if City == 'Hyderabad' else 0],
+        'City_Kolkata': [1 if City == 'Kolkata' else 0],
+        'City_Delhi': [1 if City == 'Delhi' else 0],
+        'Furnishing_Status_Semi-Furnished': [1 if Furnishing_Status == 'Semi-Furnished' else 0],
+        'Furnishing_Status_Unfurnished': [1 if Furnishing_Status == 'Unfurnished' else 0],
+        'Furnishing_Status_Furnished': [1 if Furnishing_Status == 'Furnished' else 0],
+        'Tenant_Preferred_Family': [1 if Tenant_Preferred == 'Family' else 0],
+        'Tenant_Preferred_Bachelor': [1 if Tenant_Preferred == 'Bachelor' else 0],
+        'Tenant_Preferred_Company': [1 if Tenant_Preferred == 'Company' else 0],
+        'Point_of_Contact_Agent': [1 if Point_of_Contact == 'Agent' else 0],
+        'Point_of_Contact_Owner': [1 if Point_of_Contact == 'Owner' else 0],
+        'Point_of_Contact_Builder': [1 if Point_of_Contact == 'Builder' else 0],
+        'Floors_Ground': [1 if Floors == 'Ground' else 0],
+        'Floors_Lower': [1 if Floors == 'Lower' else 0],
+        'Floors_Upper': [1 if Floors == 'Upper' else 0]
+    }
+    
+    for i in range(1, 101):
+        data[f'Floors_{i}'] = [1 if Floors == i else 0]
+    
+    data = pd.DataFrame(data)
     return data
 
 # Get user input
@@ -29,57 +65,10 @@ st.subheader('User Input Parameters')
 st.write(df.style.set_properties(**{'text-align': 'center'}))
 
 # Load the saved model
-stock = pkl.load(open('model_predict.pkl', 'rb'))
+model = pkl.load(open('model_predict.pkl', 'rb'))
 
 # Predict using the model
-prediction = stock.predict(df)
+prediction = model.predict(df)
 
 st.subheader('Prediction')
-st.write(f"Predicted Close Price: {prediction[0]:.2f}")
-
-st.divider()
-
-# Accuracy Comparison (Optional)
-st.subheader('Upload a Test Dataset for Data Analysis & Accuracy Comparison')
-uploaded_file = st.file_uploader("Upload CSV", type=["csv"])
-
-if uploaded_file:
-    # Load uploaded dataset
-    test_data = pd.read_csv(uploaded_file)
-    feature_data = test_data.copy()
-    feature_data["Date"] = pd.to_datetime(feature_data["Date"], utc=True)
-    feature_data["Date"] = feature_data["Date"].dt.tz_convert(None)
-
-    feature_data["Year"] = feature_data["Date"].dt.year
-    yearly_data = feature_data.groupby("Year")["Close"].mean()
-
-    st.subheader('Yearly Average Close Price')
-    st.line_chart(yearly_data, use_container_width=True, x_label='Year', y_label='Close Price')
-
-    # Ensure required columns exist
-    required_columns = ['Open', 'High', 'Low', 'Close']
-    if all(col in test_data.columns for col in required_columns):
-        # Separate features and actual values
-        X_test = test_data[['Open', 'High', 'Low']]
-        y_actual = test_data['Close']
-        
-        # Make predictions
-        y_pred = stock.predict(X_test)
-
-        # Calculate metrics
-        mae = (mean_absolute_error(y_actual, y_pred))*100
-        r2 = (r2_score(y_actual, y_pred))*100
-
-        # Display metrics
-        st.subheader('Model Accuracy Metrics')
-        st.write(f"Mean Absolute Error (MAE): {mae:.4f}%")
-        st.write(f"Root Squared (R^2): {r2:.4f}%")
-
-        # Display the actual and predicted values
-        st.subheader('Actual vs Predicted Values')
-        df = pd.DataFrame({'Actual': y_actual, 'Predicted': y_pred})
-        st.write(df.style.set_properties(**{'text-align': 'center'}))
-        
-    else:
-        st.error("The uploaded dataset must contain the following columns: Open, High, Low, Volume, Close")
-
+st.write(prediction)

fd528d46ffdf28d0c209c602b7c53ad53a8e4b1a
Lsy2006-github <lianyang12lol@gmail.com>
2025-01-22 01:17:07 +0800

    frontend

--- house_rental_app.py
+++ house_rental_app.py
@@ -80,10 +80,11 @@ if st.sidebar.button('Predict'):
         bar.progress(i * 2)
         time.sleep(0.1)
 
+    # Show the user input parameters
     st.subheader('User Input Parameters')
     # Transpose the dataframe to show columns as rows
     df_transposed = df.T
-    st.dataframe(df_transposed, use_container_width=True, width=800, height=400)
+    st.dataframe(df_transposed[df_transposed[0] != 0], use_container_width=True, width=800, height=400)
 
     # Load the saved model
     model = joblib.load(open('model_predict.pkl', 'rb'))
